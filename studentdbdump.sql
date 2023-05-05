875539d0185f2569965874cfd51680134b45164f
Shreyas <115518844+shreyasmg46@users.noreply.github.com>
2023-05-05 16:10:19 +0530

    Add files via upload

---
+++ studentdbdump.sql
@@ -0,0 +1,127 @@
+-- MySQL dump 10.16  Distrib 10.2.12-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: studentdb
+-- ------------------------------------------------------
+-- Server version	10.2.12-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `address` (
+  `addid` int(11) NOT NULL AUTO_INCREMENT,
+  `street` varchar(25) NOT NULL,
+  `city` varchar(25) NOT NULL,
+  `srollno` int(11) DEFAULT NULL,
+  PRIMARY KEY (`addid`),
+  KEY `srollno` (`srollno`),
+  CONSTRAINT `address_ibfk_1` FOREIGN KEY (`srollno`) REFERENCES `student` (`rollno`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` VALUES (8,'6th Cross, Ashok Nagar','Tumkur',101);
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `deptid` int(11) NOT NULL,
+  `deptname` varchar(25) NOT NULL,
+  PRIMARY KEY (`deptid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (101,'HR'),(102,'IT'),(103,'Developer'),(104,'Accounts'),(105,'DevOps'),(106,'Marketing'),(107,'Support'),(108,'Legal Advisor');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `rollno` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`rollno`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (101,'Shreyas'),(106,'Srinivas'),(300,'Shreyas'),(400,'Ravi'),(404,'Sharath'),(455,'Ijaz'),(456,'sada'),(505,'Kiran'),(600,'Deepak'),(606,'Anil'),(644,'Naveen'),(767,'Srikanth'),(788,'Adarsh'),(855,'Kiran'),(888,'Kiri'),(910,'Bilva');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentlist`
+--
+
+DROP TABLE IF EXISTS `studentlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `studentlist` (
+  `rollno` int(11) NOT NULL,
+  `sname` varchar(25) NOT NULL,
+  PRIMARY KEY (`rollno`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentlist`
+--
+
+LOCK TABLES `studentlist` WRITE;
+/*!40000 ALTER TABLE `studentlist` DISABLE KEYS */;
+INSERT INTO `studentlist` VALUES (101,'Shreyas'),(102,'Anil'),(103,'Kiran'),(104,'Srinivas'),(105,'Aditya'),(106,'Bilva'),(107,'Ranjan'),(110,'Laxman'),(122,''),(123,''),(124,''),(944,'Subbu');
+/*!40000 ALTER TABLE `studentlist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-05 16:04:40
